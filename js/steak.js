5f4e375bbfbe0c38c5e3a4f61bef2a021312f062
Hailey Strauss <hstrauss2001@gmail.com>
2025-07-06 17:40:05 -0400

    fixing issue with vide

--- js/steak.js
+++ js/steak.js
@@ -25,69 +25,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Video playback functionality
   const video = document.getElementById("myVideo");
-
-  // Function to detect mobile devices
-  function isMobileDevice() {
-    return (
-      typeof window.orientation !== "undefined" ||
-      navigator.userAgent.indexOf("IEMobile") !== -1
-    );
-  }
-
-  // Function to detect Chrome mobile
-  function isChromeMobile() {
-    return (
-      /Android/i.test(navigator.userAgent) &&
-      /Chrome/i.test(navigator.userAgent)
-    );
-  }
 
   if (video) {
     video.muted = true;
     video.loop = true;
     video.playsinline = true;
-
-    // Handle Chrome mobile specifically
-    if (isChromeMobile()) {
-      // Add a click handler for Chrome mobile
-      document.body.addEventListener(
-        "click",
-        function (e) {
-          if (e.target !== video) {
-            // First click anywhere will try to play the video
-            video.play().catch(() => {});
-          }
-        },
-        { once: true }
-      );
-    } else if (isMobileDevice()) {
-      // For other mobile devices
-      video.play().catch((error) => {
-        // Try again after a delay
-        setTimeout(() => {
-          video.play().catch(() => {});
-        }, 1000);
-      });
-
-      // Add touchstart handler
-      document.addEventListener(
-        "touchstart",
-        function () {
-          video.play().catch(() => {});
-        },
-        { once: true }
-      );
-    } else {
-      // For desktop
-      video.play().catch(() => {});
-    }
-
-    // Add a click handler for the video element itself
-    video.addEventListener("click", function () {
-      video.play().catch(() => {});
-    });
+    video.play().catch(() => {});
   }
 
   // Attach the toggleMenu function to the hamburger menu click
